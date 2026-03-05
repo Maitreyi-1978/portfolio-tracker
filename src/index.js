e47714f9c63740506c5e19d349f4ab711d6d53eb
Maitreyi-1978 <tmaitreyi77@gmail.com>
2026-03-05 23:01:11 +0530

    mui interface is added

--- src/index.js
+++ src/index.js
@@ -1,18 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App";
+
+import { Auth0Provider } from "@auth0/auth0-react";
+
+// MUI imports
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+// Create MUI theme
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976d2",
+    },
+    secondary: {
+      main: "#9c27b0",
+    },
+  },
+  typography: {
+    fontFamily: "Roboto, Arial",
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <Auth0Provider
-    domain="dev-ss2qtfzv7kx40kuw.us.auth0.com"
-    clientId="B6YSTxX5HsGW0WL2T3wKCBogPB9twTdf"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-  >
-    <App />
-  </Auth0Provider>
+  <React.StrictMode>
+    <Auth0Provider
+      domain="YOUR_AUTH0_DOMAIN"
+      clientId="YOUR_AUTH0_CLIENT_ID"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Auth0Provider>
+  </React.StrictMode>
 );
